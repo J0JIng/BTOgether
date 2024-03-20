6280f798d22de763f1d7df237f3d7646a98fd3d8
Zaiqin <phuazaiqin@gmail.com>
2024-03-20 23:30:09 +0800

    Added parents Address and workplace Address

--- src/App.js
+++ src/App.js
@@ -68,9 +68,9 @@ function App() {
       email: addPrefForm.email.value,
       gender: addPrefForm.gender.value
     })
-    .then(() => { addPrefForm.reset() })
+      .then(() => { addPrefForm.reset() })
   }
-  
+
   // Delete document by email
   const deleteByEmail = () => {
     const deletePrefForm = document.querySelector('.delete')
@@ -90,13 +90,13 @@ function App() {
       })
       .catch((error) => { console.error("Error fetching documents:", error); });
   };
-  
+
   // Update document
   const updateData = () => {
     const updatePrefForm = document.querySelector('.update')
     const email = updatePrefForm.oldemail.value
     const q = query(colRef, where("email", "==", email));
-    
+
     getDocs(q)
       .then((snapshot) => {
         // Check if a document exists
@@ -107,85 +107,101 @@ function App() {
         updateDoc(doc, {
           email: updatePrefForm.email.value,
           gender: updatePrefForm.gender.value
-        }).then(() => { updatePrefForm.reset(); console.log("Document updated successfully!");})
+        }).then(() => { updatePrefForm.reset(); console.log("Document updated successfully!"); })
           .catch((error) => { console.error("Error updating document:", error); });
       })
       .catch((error) => { console.error("Error fetching documents:", error); });
   }
-  
+
   return (
     <div className="App">
 
-    {/* Map Div */}
-    <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px'}}>
-      <GeojsonMapComponent />
-    </div>
+      {/* Map Div */}
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+        <GeojsonMapComponent />
+      </div>
 
-    <hr />
-    {/* Auth Component */}
-    <Auth />
-    <hr />
-    {/* Database Editing */}
-    <h3>Add Data</h3>
-    <form className='add' onSubmit={(e) => { e.preventDefault(); addData(); }}>
-      <label htmlFor="email">Email: </label>
-      <input type='text' name="email" required></input>
-      <label htmlFor="gender" style={{ marginLeft: '10px' }}>Gender: </label>
-      <input type='text' name="gender" required></input>
-      <button style={{ marginLeft: '10px'}}>Add a new data</button>
-    </form>
+      <hr />
+      {/* Auth Component */}
+      <Auth />
+      <hr />
+      {/* Database Editing */}
+      <h3>Add Data</h3>
+      <form className='add' onSubmit={(e) => { e.preventDefault(); addData(); }}>
+        <label htmlFor="email">Email: </label>
+        <input type='text' name="email" required></input>
+        <label htmlFor="gender" style={{ marginLeft: '10px' }}>Gender: </label>
+        <input type='text' name="gender" required></input>
+        <button style={{ marginLeft: '10px' }}>Add a new data</button>
+      </form>
 
-    <h3>Delete Data</h3>
-    <form className='delete' onSubmit={(e) => { e.preventDefault(); deleteByEmail(); }}>
-      <label htmlFor="email">Email: </label>
-      <input type='text' name="email" required></input>
-      <button style={{ marginLeft: '10px'}}>Delete data</button>
-    </form>
+      <h3>Delete Data</h3>
+      <form className='delete' onSubmit={(e) => { e.preventDefault(); deleteByEmail(); }}>
+        <label htmlFor="email">Email: </label>
+        <input type='text' name="email" required></input>
+        <button style={{ marginLeft: '10px' }}>Delete data</button>
+      </form>
 
-    <h3>Update Data</h3>
-    <form className='update' onSubmit={(e) => { e.preventDefault(); updateData(); }}>
-      <label htmlFor="oldemail">Email: </label>
-      <input type='text' name="oldemail" required></input>
-      <label htmlFor="email" style={{ marginLeft: '10px' }}>New Email: </label>
-      <input type='text' name="email" required></input>
-      <label htmlFor="gender" style={{ marginLeft: '10px' }}>New Gender: </label>
-      <input type='text' name="gender" required></input>
-      <button style={{ marginLeft: '10px' }}>Update data</button>
-    </form>
-    <br />
-    
-    <hr />
-    {/* Display Database Contents */}
-    <h2 style={{ marginBottom: '5px'}}>List of Users Preferences</h2>
-    <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px'}}>
-    {prefs && prefs.length > 0 && (
-      <table>
-        <thead>
-          <tr>
-            {Object.keys(prefs[0]).map((key) => (
-              <th key={key}>{key}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {prefs.map((pref) => (
-            <tr key={pref.id}>
-              {Object.keys(pref).map((key) => (
-                <td key={key}>{pref[key]}</td>
+      <h3>Update Data</h3>
+      <form className='update' onSubmit={(e) => { e.preventDefault(); updateData(); }}>
+        <label htmlFor="oldemail">Email: </label>
+        <input type='text' name="oldemail" required></input>
+        <label htmlFor="email" style={{ marginLeft: '10px' }}>New Email: </label>
+        <input type='text' name="email" required></input>
+        <label htmlFor="gender" style={{ marginLeft: '10px' }}>New Gender: </label>
+        <input type='text' name="gender" required></input>
+        <button style={{ marginLeft: '10px' }}>Update data</button>
+      </form>
+      <br />
+
+      <hr />
+      {/* Display Database Contents */}
+      <h2 style={{ marginBottom: '5px' }}>List of Users Preferences</h2>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '10px' }}>
+        {prefs && prefs.length > 0 && (
+          <table style={{ borderCollapse: 'collapse', border: '1px solid black', padding: '2px' }}>
+            <thead>
+              <tr>
+                {Object.keys(prefs[0]).map((key) => (
+                  <th style={{ border: '1px solid black', padding: '2px' }} key={key}>{key}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {prefs.map((pref) => (
+                <tr key={pref.id}>
+                  {Object.keys(pref).map((key) => (
+                    <td style={{ border: '1px solid black', padding: '5px' }} key={key}>
+                      {/* Check if the value is not null, undefined, or an empty string */}
+                      {pref[key] !== null && pref[key] !== undefined && pref[key] !== '' &&
+                        // Check if the value is an object
+                        typeof pref[key] === 'object' &&
+                        // Check if the object has specific keys (longitude, latitude, road)
+                        'longitude' in pref[key] && 'latitude' in pref[key] && 'road' in pref[key] ? (
+                          <div>
+                            <p>Latitude: {pref[key].latitude}</p>
+                            <p>Longitude: {pref[key].longitude}</p>
+                            <p>Road: {pref[key].road}</p>
+                          </div>
+                        )
+                        : // Render the value directly if it's not an object with specific keys
+                        pref[key]}
+                    </td>
+                  ))}
+                </tr>
               ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )}
+            </tbody>
+          </table>
+        )}
+
+
+      </div>
+
+      <hr />
+      <UserProfileForm />
 
     </div>
-    
-    <hr />
-    <UserProfileForm />
+  );
+}
 
-    </div>
-    );
-  }
-  
-  export default App;
+export default App;
--- src/components/userProfileForm.js
+++ src/components/userProfileForm.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth } from '../utils/firebase';
 import { getFirestore, collection, addDoc, updateDoc, getDocs } from 'firebase/firestore';
 import { query, where } from 'firebase/firestore';
+import axios from 'axios'; // Import axios
 
 const UserProfileForm = () => {
   // init services
   const db = getFirestore();
   // Users_pref collection ref
   const colRef = collection(db, 'User_prefs');
+  const [errorMessage, setErrorMessage] = useState(''); // Define errorMessage state
 
   const [formData, setFormData] = useState({
     maritalStatus: '',
@@ -18,46 +20,117 @@ const UserProfileForm = () => {
   
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (value || value === 0 || value === '') { // Check if value is truthy, zero, or empty string
+    if (value || value !== 0 || value !== '') { // Check if value is truthy, zero, or empty string
       setFormData({ ...formData, [name]: value });
     }
   };
 
-  const handleSubmit = (e) => {
+  useEffect(() => {
+    if (formData.parentsAddress === '' || formData.workplaceLocation === '') {
+      setErrorMessage('')
+    }
+  }, [formData.parentsAddress, formData.workplaceLocation])
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const q = query(colRef, where("email", "==", auth.currentUser.email));
   
-    getDocs(q)
-      .then((snapshot) => {
-        if (!snapshot.empty) {
-          const doc = snapshot.docs[0].ref;
-          const updateData = {};
-          for (const [key, value] of Object.entries(formData)) {
-            if (value || value === 0) { // Check if value is truthy or zero
-              updateData[key] = value;
-            }
-          }
-          updateDoc(doc, updateData)
-            .then(() => { console.log("Updated User Preferences!"); })
-            .catch((error) => { console.error("Error updating document:", error); });
-        } else {
-          console.log("Creating document for user");
-          const addData = {
-            email: auth.currentUser.email // Accessing email property
-          };
-          for (const [key, value] of Object.entries(formData)) {
-            if (value || value === 0) { // Check if value is truthy or zero
-              addData[key] = value;
-            }
-          }
-          addDoc(colRef, addData)
-            .then(() => { console.log("Saved User Preferences!"); })
-            .catch((error) => { console.error("Error adding document:", error); });
+    // Initialize an object to store updated data
+    let updatedData = {};
+  
+    // Iterate through formData to find changed fields
+    for (const key in formData) {
+      if (formData.hasOwnProperty(key)) {
+        const fieldValue = formData[key];
+        if (fieldValue && fieldValue.trim() !== '') {
+          // Field has value, add it to updatedData
+          updatedData[key] = fieldValue;
         }
-      })
-      .catch((error) => { console.error("Error fetching document:", error); });
+      }
+    }
+  
+    // Check if any field is updated
+    if (Object.keys(updatedData).length === 0) {
+      setErrorMessage('Please provide either Parents Address or Workplace Location.');
+      return;
+    }
+  
+    try {
+      // Geocode the address for each updated field
+      for (const key in updatedData) {
+        if (updatedData.hasOwnProperty(key) && (key === 'parentsAddress' || key === 'workplaceLocation')) {
+          console.log(key)
+          const addressToGeocode = updatedData[key];
+          const geocodedAddress = await geocodeAddress(addressToGeocode);
+          if (geocodedAddress === null) {
+            setErrorMessage('Error geocoding Parents or Workplace Address')
+            // Remove the key from updatedData
+            delete updatedData[key];
+          } else {
+            updatedData[key] = geocodedAddress; // Replace address with geocoded data
+          }
+        }
+      }
+    
+      // Continue with updating or adding document
+      const snapshot = await getDocs(q);
+      if (!snapshot.empty) {
+        const doc = snapshot.docs[0].ref;
+        updateDoc(doc, updatedData)
+          .then(() => { console.log("Updated User Preferences!"); })
+          .catch((error) => { console.error("Error updating document:", error); });
+      } else {
+        console.log("Creating document for user");
+        const addData = { email: auth.currentUser.email, ...updatedData };
+        addDoc(colRef, addData)
+          .then(() => { console.log("Saved User Preferences!"); })
+          .catch((error) => { console.error("Error adding document:", error); });
+      }
+    } catch (error) {
+      setErrorMessage('Error geocoding address');
+      console.error('Error geocoding address:', error);
+    }
+    
   };
   
+  
+  // Function to geocode the address
+  const geocodeAddress = async (address) => {
+    try {
+      const geocodingResponse = await axios.get(`https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(address)}&format=json&addressdetails=1&limit=1`);
+      if (geocodingResponse.data.length > 0) {
+        const { lat, lon } = geocodingResponse.data[0];
+        const latitude = parseFloat(lat).toFixed(5);
+        const longitude = parseFloat(lon).toFixed(5);
+        const road = geocodingResponse.data[0].address.road || geocodingResponse.data[0].address.suburb || geocodingResponse.data[0].address.postcode;
+  
+        const singaporeBounds = {
+          north: 1.5, south: 1.1, east: 104.1, west: 103.6
+        };
+  
+        if (
+          latitude >= singaporeBounds.south &&
+          latitude <= singaporeBounds.north &&
+          longitude >= singaporeBounds.west &&
+          longitude <= singaporeBounds.east
+        ) {
+          return {
+            latitude,
+            longitude,
+            road
+          };
+        } else {
+          setErrorMessage('The location is outside Singapore. Please refine your search.');
+        }
+      } else {
+        setErrorMessage('Address not found');
+      }
+    } catch (error) {
+      setErrorMessage('Error geocoding address');
+      console.error('Error geocoding address:', error);
+    }
+    return null; // Return null if geocoding fails
+  };
 
   return (
     <div>
@@ -88,6 +161,7 @@ const UserProfileForm = () => {
           Workplace Location:
           <input name="workplaceLocation" value={formData.workplaceLocation} onChange={handleChange} />
         </label>
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
         <br />
         <button type="submit">Submit</button>
         <br/>
