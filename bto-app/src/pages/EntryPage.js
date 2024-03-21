c3d9ee57fd9a4c4019d437444933828674719409
Your Name <you@example.com>
2024-03-22 01:45:27 +0800

    removed useless comments

--- bto-app/src/pages/EntryPage.js
+++ bto-app/src/pages/EntryPage.js
@@ -12,36 +12,3 @@ const EntryPage = () => {
  
 export default EntryPage;   
 
-
-// const TitleAndBox = styled.div`
-//   margin-top: 60px;
-//   display: flex;
-//   flex-direction: column;
-//   align-items: center;
-//   justify-content: center;
-//   gap: 60px;
-
-//   .title-and-sign-up {
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//     color: rgb(55, 65, 81);
-
-//     .title {
-//         font-weight: 700;
-//         font-size: 30px;
-//         color: rgb(55, 65, 81);
-//         min-width: 350px;
-//     }
-
-//     .register-link {
-//         color: rgb(55, 65, 81);
-//             &:hover {
-//             color: rgb(55, 65, 81);
-//             text-decoration: none;
-//             transition-duration: 0.2s;
-//             }
-
-//     }
-//   }
-// `;
