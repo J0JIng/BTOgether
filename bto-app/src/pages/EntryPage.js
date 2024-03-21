9d766a4022b4608179b8d8e2dbde98ad60d1ff5f
Your Name <you@example.com>
2024-03-22 01:43:36 +0800

    merge loginpage html to loginpage.js with working styles

--- bto-app/src/pages/EntryPage.js
+++ bto-app/src/pages/EntryPage.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styles from '../css/loginpage.css'
 
 const EntryPage = () => {
     return (  
@@ -9,4 +10,38 @@ const EntryPage = () => {
     );
 }
  
-export default EntryPage;
+export default EntryPage;   
+
+
+// const TitleAndBox = styled.div`
+//   margin-top: 60px;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 60px;
+
+//   .title-and-sign-up {
+//     display: flex;
+//     flex-direction: column;
+//     align-items: center;
+//     color: rgb(55, 65, 81);
+
+//     .title {
+//         font-weight: 700;
+//         font-size: 30px;
+//         color: rgb(55, 65, 81);
+//         min-width: 350px;
+//     }
+
+//     .register-link {
+//         color: rgb(55, 65, 81);
+//             &:hover {
+//             color: rgb(55, 65, 81);
+//             text-decoration: none;
+//             transition-duration: 0.2s;
+//             }
+
+//     }
+//   }
+// `;
