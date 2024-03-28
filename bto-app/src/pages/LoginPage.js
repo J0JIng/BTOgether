f235e5aa0b2869be218abff4f3e15a24a50595ef
Your Name <you@example.com>
2024-03-28 20:38:29 +0800

    Copied over files from Jing branch Integrate css for the loginpage Nested AuthGoogle component inside of Login.js and Register.js instead of inside Loginpage.js Created css files for Login, Register and Loginpage.js

--- bto-app/src/pages/LoginPage.js
+++ bto-app/src/pages/LoginPage.js
@@ -3,7 +3,8 @@ import { Navigate } from 'react-router-dom';
 import Login from '../components/Login'; 
 import Register from '../components/Register'; 
 import AuthGoogle from '../components/AuthGoogle';
-import styled from 'styled-components';
+import '../css/Loginpage.css';
+
 
 
 const LoginPage = ({ user }) => {
@@ -19,28 +20,43 @@ const LoginPage = ({ user }) => {
   return (
     <div className="login-page">
       
-      <div className="navbar">
-        <h1>Login page</h1>
+      <div className="top-box">
+          <img src={process.env.PUBLIC_URL + 'btodraftlogo.png'} className="app-logo" style={{width: '60px', height: '60px', borderRadius: '5px'}} />
+          <div className="options">
+            <a className="homepage-link" href>Homepage</a>
+            {/* <a className="login-link" href>Login</a> */}
+          </div>
       </div>
 
-      <div className="welcome-section">
+      {/* <div className="welcome-section">
         <h1>Welcome to BTOgether</h1>
+      </div> */}
+
+      <div className='title-and-box'>
+        <div className="title-and-sign-up">
+            <div className="title">Welcome to BTOgether</div>
+            {isSignUpActive && <a className='loginpage redirect' onClick={handleMethodChange}>Have an account? Login here</a>}
+            {!isSignUpActive && (<a className='registerpage redirect' onClick={handleMethodChange}>Don't have an account? Register here</a>)}
+            {/* <p className="register-prefix">Or <a className="register-link" href>register for a new account</a>
+            </p> */}
+        </div>
+
+        <div className="login-page-wrapper">
+
+        {isSignUpActive ? (
+              <Register user={user} handleMethodChange={handleMethodChange} />
+            ) 
+            
+            : (
+              <Login user={user} handleMethodChange={handleMethodChange} />
+            )}  
+
+
+        </div> 
       </div>
 
-      <div className="login-page-wrapper">
-
-      {isSignUpActive ? (
-            <Register user={user} handleMethodChange={handleMethodChange} />
-          ) : (
-            <Login user={user} handleMethodChange={handleMethodChange} />
-          )}  
-
-          <AuthGoogle user={user}/>
-
-      </div>
-
-      {isSignUpActive && <a onClick={handleMethodChange}>Login</a>}
-      {!isSignUpActive && (<a onClick={handleMethodChange}>Dont have an account? Create here</a>)}
+      {/* {isSignUpActive && <a className='loginpage-redirect' onClick={handleMethodChange}>Login</a>}
+      {!isSignUpActive && (<a className='registerpage-redirect' onClick={handleMethodChange}>Dont have an account? Create here</a>)} */}
 
     </div>
   );
