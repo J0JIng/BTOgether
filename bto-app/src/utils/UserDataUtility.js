3fe351f5865565ca9a9b3d03e6c1c4d84c346de8
Zaiqin <phuazaiqin@gmail.com>
2024-04-13 15:24:33 +0800

    Added documentation for UserDataUtility

--- bto-app/src/utils/UserDataUtility.js
+++ bto-app/src/utils/UserDataUtility.js
@@ -10,6 +10,36 @@ import {
   updateDoc,
 } from "firebase/firestore";
 
+/**
+ * A utility component for managing user data in Firestore.
+ * It provides functions to save and load user preferences.
+ *
+ * @param {Object} props - The component's props.
+ * @param {Object} props.saveData - An object that will be saved by UserDataUtility.
+ * @param {Function} props.loadedData - A function to receive loaded user data.
+ * @param {React.RefObject} ref - The ref forwarded to the outermost element. 
+ * Use 'useRef' in parent components to reference this component
+ * @returns {JSX.Element} A JSX element representing the UserDataUtility component.
+ * 
+ * Create the component in this manner in a parent function
+ * <UserDataUtility ref={dataUtilityRef} saveData={dataToBeSaved} loadedData={handleLoadedData} />
+ * 
+ * To save data, in the parent component, call ref.current.saveUserData(). 
+ * The object placed into props.saveData will be saved into Firestore
+ * 
+ * To load data, in the parent component, call ref.current.loadUserData(). 
+ * The loaded data will be available in the function passed in prop.loadedData() as 'data'. For example:
+ * const handleLoadedData = (data) => {
+    if (data) {
+      console.log("Loaded from firestore:", data);
+      setLoadedData(data);
+    } else {
+      console.log("No data found"); 
+    }
+  };
+ * 
+ */
+
 const UserDataUtility = forwardRef(({ saveData, loadedData }, ref) => {
   const db = getFirestore();
   // Users_pref collection ref
